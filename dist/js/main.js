b5af2e4c0775929ceffc2c39c6db8f610fdb9bc1
maher1983 <maher1983@hotmail.com>
2020-08-23 02:09:39 +0300

    Lesson 18

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,6 +10,41 @@ $(function () {
             .next('.child-links')
             .slideToggle();
     });
+    // Open / Close Fullscreen
+    $('.toggle-fullscreen').on('click', function () {
+        $(this).toggleClass('full-screen');
+        if ($(this).hasClass('full-screen')) { // Page Is FullScrenn
+            openFullscreen();
+        } else { // Page Is Not FullScreen
+            closeFullscreen();
+        }
+    });
 });
+
+var elem = document.documentElement;
+
+function openFullscreen() {
+if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+}
+}
+
+function closeFullscreen() {
+if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+}
+}
 const myMap = new Map();
 const mySet = new Set();
--- stage/js/main.js
+++ stage/js/main.js
@@ -10,4 +10,39 @@ $(function () {
             .next('.child-links')
             .slideToggle();
     });
+    // Open / Close Fullscreen
+    $('.toggle-fullscreen').on('click', function () {
+        $(this).toggleClass('full-screen');
+        if ($(this).hasClass('full-screen')) { // Page Is FullScrenn
+            openFullscreen();
+        } else { // Page Is Not FullScreen
+            closeFullscreen();
+        }
+    });
 });
+
+var elem = document.documentElement;
+
+function openFullscreen() {
+if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+}
+}
+
+function closeFullscreen() {
+if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+}
+}
